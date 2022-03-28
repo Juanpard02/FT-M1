4d84778fe9ac9ccf9be0f6d2820f7fd72ff82f14
Juan Pablo <juanrossi02@hotmail.com.ar>
2022-03-28 16:57:01 -0300

    primer commit

--- 00-IntroToCS/homework/homework.js
+++ 00-IntroToCS/homework/homework.js
@@ -2,12 +2,17 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
+    if (num.constructor != String) {
+      return null;
+    }
 
+    return Number.parseInt(num, 2);
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+  
+  return num.toString(2);
 }
 
 module.exports = {
